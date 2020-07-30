5141338e8568860de797c5973cad04a5dedef91b
leonardeyer <le0nard@me.com>
2020-07-30 17:50:17 +0200

    Add more comments

--- source/projects/pa.string_tilde/StringVibration.c
+++ source/projects/pa.string_tilde/StringVibration.c
@@ -8,6 +8,7 @@
 
 #define M_PI_POW_2 9.8696044011f
 
+// ######### Helper functions #############
 float heavside(float x) {
   return (float) (x >= 0.f);
 }
@@ -19,6 +20,7 @@ float k(int i, float L) {
 float w(int i, float alpha, float c_0, float L) {
   return sqrtf(powf(k(i, L) * c_0, 2.f) - powf(alpha, 2.f));
 }
+// #######################################
 
 float phase_velocity(string s) {
   return 2 / s.diameter * sqrtf((float) (s.tension / M_PI * s.density));
@@ -29,21 +31,27 @@ float base_freq(string s) {
 }
 
 float u(string s, float x, float t) {
+  // Number of frequencies to compute
   int nFreq = 10;
+  // The damping factor
   float alpha = 0.2f;
+  // TODO: What is this
   float h_0 = 0.2f;
+  // Normalized position of plucking
   float b = x / s.length;
+  // Phase velocity
   float c_0 = phase_velocity(s);
+  // Length
   float L = s.length;
 
+  // Wave equation sampling (https://de.wikipedia.org/wiki/Saitenschwingung#Spektralanalyse_(Fouriertransformation))
   float sum = 0.f;
   for (int i = 1; i <= nFreq; ++i) {
-
     float part = (float) 2.0f * h_0 * sinf(M_PI * b * i) * sinf(k(i, L) * x) * cosf(w(i, alpha, c_0, L) * t);
     part /= M_PI_POW_2 * powf((float) i, 2.f) * b * L * (1 - b);
     sum += part;
   }
-  sum *= powf(M_E, -alpha * t) * heavside(t);
+  sum *= powf(M_E, -alpha * t);
 
   return sum;
 }
--- source/projects/pa.string_tilde/StringVibration.h
+++ source/projects/pa.string_tilde/StringVibration.h
@@ -17,10 +17,27 @@ typedef struct string {
 
 } string;
 
+/**
+ * For given string compute it's phase velocity
+ * @param s the string
+ * @return the velocity
+ */
 float phase_velocity(string s);
 
+/**
+ * Base frequency of the string vibration
+ * @param s the string
+ * @return lowest frequency
+ */
 float base_freq(string s);
 
+/**
+ * Evaluation of the wave equation
+ * @param s string
+ * @param x position of plucking
+ * @param t time
+ * @return string displacement
+ */
 float u(string s, float x, float t);
 
 #endif //DRUM_SHAPES_SOURCE_PROJECTS_PA_STRING_TILDE_STRINGVIBRATION_H_
